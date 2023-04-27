37c1f6fffa7f593789822c630acdce966241e21e
Approximations <datkidbosst@gmail.com>
2023-04-27 16:14:41 -0300

    added loading spinner

--- part1/q1/src/components/UsersTable/index.tsx
+++ part1/q1/src/components/UsersTable/index.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { User } from '../constants/types/User';
 import styles from './index.module.css';
+import { FaSpinner } from 'react-icons/fa';
 
 // Create a ReactJS component that displays a list of users retrieved from the following API endpoint: https://jsonplaceholder.typicode.com/users. The component should display the user's name, email, and phone number. Use the fetch() method to retrieve the data from the API.
 export default function Users() {
     const [users, setUsers] = useState<Array<User>>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((res) => {
+                setLoading(false);
                 if (!res.ok) throw new Error('Could not fetch data.');
 
                 return res.json();
@@ -32,17 +36,24 @@ export default function Users() {
                 </thead>
 
                 <tbody>
-                    {users.map((user) => {
-                        return (
-                            <tr>
-                                <td className={styles.name}>{user.name}</td>
-                                <td className={styles.email}>{user.email}</td>
-                                <td className={styles.phone}>{user.phone}</td>
-                            </tr>
-                        );
-                    })}
+                    {!loading &&
+                        users.map((user) => {
+                            return (
+                                <tr key={user.email}>
+                                    <td className={styles.name}>{user.name}</td>
+                                    <td className={styles.email}>{user.email}</td>
+                                    <td className={styles.phone}>{user.phone}</td>
+                                </tr>
+                            );
+                        })}
                 </tbody>
             </table>
+
+            {loading && (
+                <div className={styles.loading}>
+                    <FaSpinner></FaSpinner>
+                </div>
+            )}
         </div>
     );
 }
